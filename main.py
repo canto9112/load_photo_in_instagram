c2b5d8cc1934d8752c95f9d732ffb5396fe3ec8b
canto9112 <cantoweb112@gmail.com>
2021-01-10 18:05:22 +0300

    Привел все к 1 функции - fetch_spacex_last_launch

--- main.py
+++ main.py
@@ -3,26 +3,33 @@ from pathlib import Path
 from pprint import pprint
 
 
-def get_links_images():
-    params = {'flight_number': '108'}
-    response = requests.get('https://api.spacexdata.com/v3/launches', params=params)
+# def save_image(url, path):
+#     response = requests.get(url)
+#     response.raise_for_status()
+#     with open(path, 'wb') as file:
+#         file.write(response.content)
+
+
+def fetch_spacex_last_launch(url, flight_number, name_folder, template_file_name):
+    params = {'flight_number': flight_number}
+    response = requests.get(url, params=params)
     response.raise_for_status()
     links_image = response.json()[0]['links']['flickr_images']
-    return links_image
-
-
-def save_image(url, path):
-    response = requests.get(url)
-    response.raise_for_status()
-    with open(path, 'wb') as file:
-        file.write(response.content)
+    Path(name_folder).mkdir(parents=True, exist_ok=True)
+    for link_number, link in enumerate(links_image):
+        filename = template_file_name.format(link_number)
+        path = Path.cwd() / name_folder / filename
+        response = requests.get(link)
+        response.raise_for_status()
+        content = response.content
+        with open(path, 'wb') as file:
+            file.write(content)
 
 
 if __name__ == "__main__":
-    folder = Path("images").mkdir(parents=True, exist_ok=True)
-    last_links_images = get_links_images()
+    url = 'https://api.spacexdata.com/v3/launches'
+    flight_number = '108'
+    name_folder = "images"
+    template_file_name = 'spacex-{}.jpg'
 
-    for link_number, link in enumerate(last_links_images):
-        filename = 'spacex{}.jpg'.format(link_number)
-        path = Path.cwd() / 'images' / filename
-        save_image(link, path)
+    fetch_spacex_last_launch(url, flight_number, name_folder, template_file_name)
