96d61c2b895df95330553d89f10ade4f42ba07b3
canto9112 <cantoweb112@gmail.com>
2021-01-19 20:10:06 +0300

    Вынес создание папки в отдельную функцию

--- utils.py
+++ utils.py
@@ -2,10 +2,13 @@ import requests
 from pathlib import Path
 
 
+def create_folder(folder_name):
+    Path(folder_name).mkdir(parents=True, exist_ok=True)
+
+
 def save_image(url, image_name, folder_name):
     response = requests.get(url, verify=False)
     response.raise_for_status()
-    Path(folder_name).mkdir(parents=True, exist_ok=True)
     path = Path.cwd() / folder_name / image_name
     content = response.content
     with open(path, 'wb') as file:
