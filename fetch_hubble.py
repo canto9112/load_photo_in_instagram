faea50070580080de7afb01efc70ef2c08ba3a3e
canto9112 <cantoweb112@gmail.com>
2021-01-19 21:56:03 +0300

    Изменил названия переменных

--- fetch_hubble.py
+++ fetch_hubble.py
@@ -3,36 +3,36 @@ import os
 import utils
 
 
-def get_link_last_image(url):
+def get_image_last_link(url):
     response = requests.get(url, verify=False)
     response.raise_for_status()
     image_files = response.json()['image_files']
     for image in image_files:
-        link_last_image = ('http:'+image['file_url'])
-    return link_last_image
+        image_last_link = ('http:'+image['file_url'])
+    return image_last_link
 
 
-def get_id_images_habble(url, collection_name):
+def get_habble_image_ids(url, collection_name):
     params = {'page': 'all',
               'collection_name': collection_name}
     response = requests.get(url, params=params)
     response.raise_for_status()
     collection_contents = response.json()
-    id_images = []
-    for id_image in collection_contents:
-        id_images.append(id_image['id'])
-    return id_images
+    image_ids = []
+    for image_id in collection_contents:
+        image_ids.append(image_id['id'])
+    return image_ids
 
 
 def get_images_habble():
-    folder_saving_images = "images"
+    images_folser = "images"
     habble_collection_name = os.getenv('HABBLE_COLLECTION_NAME')
-    url_habble_collections_api = 'http://hubblesite.org/api/v3/images'
-    id_images_habble = get_id_images_habble(url_habble_collections_api, habble_collection_name)
-    for link_number, link in enumerate(id_images_habble):
-        url_habble_api = 'http://hubblesite.org/api/v3/image/{}'.format(link)
-        link_last_image = get_link_last_image(url_habble_api)
-        file_extension_image_habbble = utils.get_file_extension(link_last_image)
-        habble_image_name = (f'habble-{link_number}{file_extension_image_habbble}')
-        utils.create_folder_save_images(folder_saving_images)
-        utils.save_image(link_last_image, habble_image_name, folder_saving_images)
+    habble_collections_api_url = 'http://hubblesite.org/api/v3/images'
+    habble_image_ids = get_habble_image_ids(habble_collections_api_url, habble_collection_name)
+    for link_number, link in enumerate(habble_image_ids):
+        habble_api_url = 'http://hubblesite.org/api/v3/image/{}'.format(link)
+        image_last_link = get_image_last_link(habble_api_url)
+        file_extension = utils.get_file_extension(image_last_link)
+        habble_image_name = (f'habble-{link_number}{file_extension}')
+        utils.create_folder_save_images(images_folser)
+        utils.save_image(image_last_link, habble_image_name, images_folser)
