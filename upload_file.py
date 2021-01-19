c6f5e005e308ff453fa865f21d3c4e4f09a8f187
canto9112 <cantoweb112@gmail.com>
2021-01-19 21:13:23 +0300

    Изменил название переменных

--- upload_file.py
+++ upload_file.py
@@ -6,15 +6,15 @@ import time
 import shutil
 
 
-def adjust_and_save_image(name_folder, upload_folder):
-    content = os.listdir(path=name_folder)
+def adjust_and_save_image(folder_images, folder_upload):
+    content = os.listdir(path=folder_images)
     for file in content:
         file_extansion = file[-4:]
         file_name = file.replace(file_extansion, '')
-        image = Image.open(str(name_folder + f"/{file}"))
+        image = Image.open(str(folder_images + f"/{file}"))
         image.thumbnail((1080, 1080))
-        Path(upload_folder).mkdir(parents=True, exist_ok=True)
-        path = Path.cwd() / upload_folder / file_name
+        Path(folder_upload).mkdir(parents=True, exist_ok=True)
+        path = Path.cwd() / folder_upload / file_name
         if image.mode != 'RGB':
             ycbcr_image = image.convert('YCbCr')
             ycbcr_image.save(f'{path}.jpg', 'JPEG')
@@ -22,26 +22,26 @@ def adjust_and_save_image(name_folder, upload_folder):
             image.save(f'{path}.jpg', 'JPEG')
 
 
-def delete_folder(name_folder):
-    folder_path = Path(name_folder)
+def delete_folder(folder):
+    folder_path = Path(folder)
     shutil.rmtree(folder_path)
 
 
-def upload_file(name_folder):
-    password_inst = os.getenv('PASSWORD_INST')
-    login_inst = os.getenv('LOGIN_INST')
-    folder_contents = os.listdir(path=name_folder)
+def upload_file(folder):
+    password_inst = os.getenv('ISTAGRAM_PASSWORD')
+    login_inst = os.getenv('INSTAGRAM_LOGIN')
+    folder_contents = os.listdir(path=folder)
     bot = Bot()
     bot.login(username=login_inst, password=password_inst)
     for file in folder_contents:
-        bot.upload_photo(str(name_folder) + f'/{file}')
+        bot.upload_photo(str(folder) + f'/{file}')
         time.sleep(60)
 
 
 def start_uploading_images():
-    folder_saving_images = "images"
-    folder_uploading_instsgram = 'images_to_download'
+    folder_images = "images"
+    folder_uploading_instagram = 'images_to_download'
 
-    adjust_and_save_image(folder_saving_images, folder_uploading_instsgram)
-    delete_folder(folder_saving_images)
-    upload_file(folder_uploading_instsgram)
+    adjust_and_save_image(folder_images, folder_uploading_instagram)
+    delete_folder(folder_images)
+    upload_file(folder_uploading_instagram)
