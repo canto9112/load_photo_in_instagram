d8736cebda59da3226cb7d3f1f79b20cdb62cece
canto9112 <cantoweb112@gmail.com>
2021-01-24 17:18:33 +0300

    Высушил код

--- upload_file.py
+++ upload_file.py
@@ -8,13 +8,12 @@ import shutil
 
 def adjust_and_save_images(images_folder, upload_folder):
     filepath = os.listdir(path=images_folder)
-    for file_name in filepath:
-        name, extension = os.path.splitext(file_name)
-        image_name = file_name.replace(extension, '')
-        image = Image.open(f'{images_folder}/{file_name}')
+    for filename in filepath:
+        name, extension = os.path.splitext(filename)
+        image = Image.open(f'{images_folder}/{filename}')
         image.thumbnail((1080, 1080))
         Path(upload_folder).mkdir(parents=True, exist_ok=True)
-        path = Path.cwd() / upload_folder / image_name
+        path = Path.cwd() / upload_folder / name
         if image.mode != 'RGB':
             ycbcr_image = image.convert('YCbCr')
             ycbcr_image.save(f'{path}.jpg', 'JPEG')
@@ -42,5 +41,5 @@ def uploading_images():
     images_folder = "images"
     instagram_images_folder = 'images_to_download'
     adjust_and_save_images(images_folder, instagram_images_folder)
-    delete_folder(images_folder)
-    upload_images_instagram(instagram_images_folder)
+    #delete_folder(images_folder)
+    #upload_images_instagram(instagram_images_folder)
