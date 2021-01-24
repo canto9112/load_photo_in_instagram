4108c98defbb721b7b474ac8880cb18ae0553a3e
canto9112 <cantoweb112@gmail.com>
2021-01-24 11:13:02 +0300

    Изменил назвавния переменных

--- upload_file.py
+++ upload_file.py
@@ -7,14 +7,14 @@ import shutil
 
 
 def adjust_and_save_images(images_folder, upload_folder):
-    content = os.listdir(path=images_folder)
-    for file in content:
-        name, extension = os.path.splitext(file)
-        file_name = file.replace(extension, '')
-        image = Image.open(f'{images_folder}/{file}')
+    filepath = os.listdir(path=images_folder)
+    for file_name in filepath:
+        name, extension = os.path.splitext(file_name)
+        image_name = file_name.replace(extension, '')
+        image = Image.open(f'{images_folder}/{file_name}')
         image.thumbnail((1080, 1080))
         Path(upload_folder).mkdir(parents=True, exist_ok=True)
-        path = Path.cwd() / upload_folder / file_name
+        path = Path.cwd() / upload_folder / image_name
         if image.mode != 'RGB':
             ycbcr_image = image.convert('YCbCr')
             ycbcr_image.save(f'{path}.jpg', 'JPEG')
@@ -30,11 +30,11 @@ def delete_folder(folder):
 def upload_images_instagram(folder):
     password_inst = os.getenv('ISTAGRAM_PASSWORD')
     login_inst = os.getenv('INSTAGRAM_LOGIN')
-    content = os.listdir(path=folder)
+    filepath = os.listdir(path=folder)
     bot = Bot()
     bot.login(username=login_inst, password=password_inst)
-    for file in content:
-        bot.upload_photo(f'{folder}/{file}')
+    for file_name in filepath:
+        bot.upload_photo(f'{folder}/{file_name}')
         time.sleep(60)
 
 
