09aed5939ad183db56e839eda0447acd3dbf65de
canto9112 <cantoweb112@gmail.com>
2021-01-26 17:40:41 +0300

    Переименовал переменную

--- upload_file.py
+++ upload_file.py
@@ -6,9 +6,9 @@ import time
 def upload_images_instagram(folder):
     password_inst = os.getenv('ISTAGRAM_PASSWORD')
     login_inst = os.getenv('INSTAGRAM_LOGIN')
-    filepath = os.listdir(path=folder)
+    filenames = os.listdir(path=folder)
     bot = Bot()
     bot.login(username=login_inst, password=password_inst)
-    for file_name in filepath:
-        bot.upload_photo(f'{folder}/{file_name}')
+    for filename in filenames:
+        bot.upload_photo(f'{folder}/{filename}')
         time.sleep(60)
