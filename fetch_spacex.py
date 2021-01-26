2425ab74129a515a9b847c3f0252c186eade9e9e
canto9112 <cantoweb112@gmail.com>
2021-01-26 18:38:44 +0300

    Изменил название аргумента функции

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -10,10 +10,10 @@ def fetch_spacex_launch(url, flight_number):
     return links_images
 
 
-def fetch_spacex_images(name_folder_save, spacex_flight_number='220'):
+def fetch_spacex_images(folder_name, spacex_flight_number='220'):
     url_spacex_api = 'https://api.spacexdata.com/v3/launches'
     spacex_template_file_name = 'spacex-{}.jpg'
     spacex_last_launch_url = fetch_spacex_launch(url_spacex_api, spacex_flight_number)
     for link_number, link in enumerate(spacex_last_launch_url):
         spacex_image_name = spacex_template_file_name.format(link_number)
-        utils.save_image(link, spacex_image_name, name_folder_save)
+        utils.save_image(link, spacex_image_name, folder_name)
