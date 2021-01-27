98a316ce20d12d5493344f550e8498fdb741e571
canto9112 <cantoweb112@gmail.com>
2021-01-27 17:47:56 +0300

    Переименовал функцию

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -2,7 +2,7 @@ import requests
 import utils
 
 
-def fetch_spacex_launch(url, flight_number):
+def fetch_spacex_launch_urls(url, flight_number):
     params = {'flight_number': flight_number}
     response = requests.get(url, params=params)
     response.raise_for_status()
@@ -13,7 +13,7 @@ def fetch_spacex_launch(url, flight_number):
 def fetch_spacex_images(folder_name, spacex_flight_number='220'):
     url_spacex_api = 'https://api.spacexdata.com/v3/launches'
     filename_template = 'spacex-{}.jpg'
-    spacex_last_launch_url = fetch_spacex_launch(url_spacex_api, spacex_flight_number)
+    spacex_last_launch_url = fetch_spacex_launch_urls(url_spacex_api, spacex_flight_number)
     for link_number, link in enumerate(spacex_last_launch_url):
         spacex_image_name = filename_template.format(link_number)
         utils.save_image(link, spacex_image_name, folder_name)
